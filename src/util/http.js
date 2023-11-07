1f5cfe8a9e6eb5690683bbcd24b6f315b4c174df
Arron <beale86@msn.com>
2023-11-07 21:08:00 +0000

    Add fetchEvent and deleteEvent functions to http.js

--- src/util/http.js
+++ src/util/http.js
@@ -46,6 +46,7 @@ export async function createNewEvent(eventData) {
   return event;
 }
 
+
 export async function fetchSelectableImages({ signal }) {
   const response = await fetch(`https://3000-arronbeale-reacttanstac-ce623wqpb30.ws-eu105.gitpod.io/events/images`, { signal });
 
@@ -59,4 +60,36 @@ export async function fetchSelectableImages({ signal }) {
   const { images } = await response.json();
 
   return images;
+}
+
+
+export async function fetchEvent({ id, signal }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, { signal });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while fetching the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
+}
+
+
+export async function deleteEvent({ id }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    method: 'DELETE',
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while deleting the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  return response.json();
 }
